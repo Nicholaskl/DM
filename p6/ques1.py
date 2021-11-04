3116c7f2aa6d95451aa33d25ea1e1d077081e693
Nicholas Kl <nicholasklvanahooper@gmail.com>
2021-11-04 10:10:47 +0800

    clustering prac done

--- p6/ques1.py
+++ p6/ques1.py
@@ -10,22 +10,41 @@ from sklearn import metrics
 from sklearn.datasets import make_blobs
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics.pairwise import pairwise_distances_argmin
 
 df = pd.read_csv('bmw-browsers.csv')
+X = df.values
 
-k = 5
-
+num_k = 5
 # run kmeans
 # https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
-kmeans = KMeans(n_clusters=k,random_state=0).fit(df)
+kmeans = KMeans(n_clusters=num_k,random_state=0).fit(X)
 
 # extract centroids
 clusters = kmeans.cluster_centers_
 
 # instance counts in each cluster
 counts = np.bincount(kmeans.labels_)
-df.plot.scatter('x', 'y')
-# plt.scatter(df[:, 0], df[:, 1], c=kmeans)
 
+colors = ["#4EACC5", "#FF9C34", "#4E9A06", "#BB34FF", "#FF346E"]
 
+k_means_labels = pairwise_distances_argmin(X, clusters)
+
+fig = plt.figure(figsize=(8, 3))
+
+# KMeans
+ax = fig.add_subplot(1, 3, 1)
+for k, col in zip(range(num_k), colors):
+    my_members = k_means_labels == k
+    cluster_center = clusters[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], "w", markerfacecolor="#bbbbbb", marker=".")
+    ax.plot(
+        cluster_center[0],
+        cluster_center[1],
+        "o",
+        markerfacecolor=col,
+        markeredgecolor="k",
+        markersize=6,
+    )
+ax.set_title("KMeans")
 # %%
